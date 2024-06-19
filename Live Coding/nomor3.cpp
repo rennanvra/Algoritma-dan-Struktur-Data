2e077c61d700309515c496cbed76097d73ef5fde
rennanvra <renna.novaria.12@gmail.com>
2024-06-19 10:20:12 +0700

    alpro strukdat

---
+++ Live Coding/nomor3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int angka;
+
+    Node *next;
+}*head,*tail,*cur,*newNode;
+
+void headData(int angka){
+    head = new Node();
+
+    Node *head;angka = angka;
+
+    head->next=NULL;
+    tail=head;
+}
+
+void tailData(int angka){
+    tail = new Node();
+
+    Node *tail;angka = angka;
+
+    tail->next=NULL;
+    newNode->next=tail;
+}
+
+void curData(int angka){
+    cur = new Node();
+
+    Node *cur;angka = angka;
+
+
+}
+
+void newNodeData(int angka){
+    newNode = new Node();
+
+    Node *newNode;angka = angka;
+
+
+}
+
+void hapusTengah(){
+    
+}
+
+int main(){
+    cout << "Maaf, saya gabisa Pak." << endl;
+}
